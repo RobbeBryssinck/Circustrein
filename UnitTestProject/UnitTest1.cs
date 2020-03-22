b9881ca66c96467f0bb6c633e6f3ee9255f3cb76
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-03-22 17:34:33 +0100

    Added first unit test

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,18 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using Circustrein;
+using System;
+using System.Collections.Generic;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
-        Train train = new Train();
-
-
         [TestMethod]
-        public void TestMethod1()
+        public void AddAnimalToWagonTest()
         {
+            Train train = new Train();
+            List<Wagon> localWagons = new List<Wagon>();
+            localWagons.Add(new Wagon());
+            localWagons[0].AddAnimal(new Animal(1, "Herbivore"));
+            train.AddWagon(0, new Animal(1, "Herbivore"));
+
+            List<Wagon> wagons = train.GetWagons();
+
+            CollectionAssert.AreEqual(wagons, localWagons);
         }
     }
 }
